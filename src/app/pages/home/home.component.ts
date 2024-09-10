9e61c4a0b0080a4d7ded46fcba34cfe00b6a7bf6
victeo <victor.atomo@gmail.com>
2024-09-10 16:38:19 -0300

    Estilo do login

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CheckboxModule} from "primeng/checkbox";
-import { ButtonModule } from 'primeng/button';
+import {ButtonModule} from 'primeng/button';
 import {CardModule} from "primeng/card";
 import {FloatLabelModule} from "primeng/floatlabel";
-import { InputTextModule } from 'primeng/inputtext';
+import {InputTextModule} from 'primeng/inputtext';
 import {PasswordModule} from "primeng/password";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 
 @Component({
@@ -16,11 +17,35 @@ import {PasswordModule} from "primeng/password";
     CardModule,
     FloatLabelModule,
     InputTextModule,
-    PasswordModule
+    PasswordModule,
+    ReactiveFormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  formControl!: FormGroup;
 
+  constructor(        private formBuilder: FormBuilder,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  /**
+   * Inicializa o formulário com os campos e validadores necessários.
+   */
+  private initializeForm(): void {
+    this.formControl = this.formBuilder.group({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+
+    });
+  }
+
+  onSubmit() {
+
+  }
 }
